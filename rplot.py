134afb1dcbf00b4ad8a490a5f6645ca25d8bac22
RongkangWang <wangrongkang@mail.nankai.edu.cn>
2017-12-28 00:05:29 +0800

    rplot class

--- rplot.py
+++ rplot.py
@@ -3,19 +3,35 @@ import random
 from keras.models import model_from_json
 import numpy as np
 
-def acc4voip(rows=100):
-	data, label = load_data(rows=rows)
-	origin_model = model_from_json(open("../data/model_json/alexnet_model_architecture_"+str(rows)+".json").read())
-	origin_model.load_weights("../data/model_json/alexnet_model_weights_"+str(rows)+".h5")
-	pred_label = origin_model.predict(data,batch_size=1, verbose=1)
-	num = len(label)
-	voip_len = [0 for i in range(7)]
-	for i in range(num):
-		if label[i]==np.argmax(pred_label[i]):
-			index = label[i].astype(np.int64)-1
-			voip_len[index] += 1
-	voip_acc = [voip_len[i]/(num/7.0) for i in range(7)]
-	print(voip_acc)
+class plotmodel:
+    def __init__(self,rows=100):
+        self.rows = rows
+        model = model_from_json(
+            open("../data/model_json/alexnet_model_architecture_" + str(rows) + ".json").read())
+        model.load_weights("../data/model_json/alexnet_model_weights_" + str(rows) + ".h5")
+        self.model = model
+        self.data ,self.label = load_data(rows=rows)
+        self.pred_label = model.predict(self.data,batch_size=1, verbose=1)
+
+    def acc4voip(self):
+        num = len(self.label)
+        voip_len = [0 for i in range(7)]
+        for i in range(num):
+            if self.label[i] == np.argmax(self.pred_label[i]):
+                index = self.label[i].astype(np.int64) - 1
+                voip_len[index] += 1
+        voip_acc = [voip_len[i] / (num / 7.0) for i in range(7)]
+        print(voip_acc)
+
+    def fprfnr(self):
+        return
+
+    def unknowpkt(self):
+        return
+
+
+
 
 if __name__=="__main__":
-	acc4voip(rows=10)
+    pmodel = plotmodel(rows=100)
+    pmodel.model.summary()
